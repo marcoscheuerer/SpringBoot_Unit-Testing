6327dca7200ab0d97fbf874e786816cb18d510ad
Marco Scheuerer <MarcoScheuerer1978@gmail.com>
2023-05-15 12:41:15 +0200

    Test Case Lifecycle Methods

--- 1.00-starting-project/src/test/java/mao/linatrix/junitdemo/DemoUtilsTest.java
+++ 1.00-starting-project/src/test/java/mao/linatrix/junitdemo/DemoUtilsTest.java
@@ -1,17 +1,42 @@
 package mao.linatrix.junitdemo;
 
 import com.luv2code.junitdemo.DemoUtils;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 public class DemoUtilsTest {
 
+    DemoUtils demoUtils;
+
+    @BeforeAll
+    static void setupBeforeEachClass() {
+        System.out.println("@BeforeAll executes only once before all test methods execution in the class");
+    }
+
+    @AfterAll
+    static void tearDownAfterAll()
+    {
+        System.out.println("@AfterAll executes only once after all methods execution in the class");
+    }
+
+    @BeforeEach
+    void setupBeforeEach() {
+        demoUtils = new DemoUtils();
+        System.out.println("@BeforeEach executes before the execution of each test method");
+    }
+
+    @AfterEach
+    void tearDownAfterEach() {
+        System.out.println("Running @AfterEach");
+    }
+
     @Test
     void testEqualsAndNotEquals() {
 
+        System.out.println("Running test: testEqualsAndNotEquals");
+
         // set up
-        DemoUtils demoUtils = new DemoUtils();
         int expected = 6;
         int unexpected = 10;
 
@@ -25,9 +50,9 @@ public class DemoUtilsTest {
 
     @Test
     void testNullAndNotNull() {
+        System.out.println("Running test: testNullAndNotNull");
 
         // set up
-        DemoUtils demoUtils = new DemoUtils();
         String str1 = null;
         String str2 = "luv2code";
 
