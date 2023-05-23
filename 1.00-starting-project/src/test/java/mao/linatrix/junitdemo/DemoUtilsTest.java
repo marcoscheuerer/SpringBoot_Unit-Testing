fab1bdceb5140c3dd48336125abbddd162ce56d8
Marco Scheuerer <MarcoScheuerer1978@gmail.com>
2023-05-23 07:00:38 +0200

    assertThrows, assertDoesNotThrow, assertTimeoutPreemptively

--- 1.00-starting-project/src/test/java/mao/linatrix/junitdemo/DemoUtilsTest.java
+++ 1.00-starting-project/src/test/java/mao/linatrix/junitdemo/DemoUtilsTest.java
@@ -2,6 +2,7 @@ package mao.linatrix.junitdemo;
 
 import org.junit.jupiter.api.*;
 
+import java.time.Duration;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -108,6 +109,26 @@ public class DemoUtilsTest {
         List<String> theList = List.of("luv", "2", "code");
 
         assertLinesMatch(theList, demoUtils.getAcademyInList(), "Lines should match");
+    }
+
+    @Test
+    @DisplayName("Throws and Does Not Throw")
+    void testThrowsAndDoesNotThrow() {
+        assertThrows(Exception.class, () -> {
+            demoUtils.throwException(-1);
+        }, "Should throw exception");
+
+        assertDoesNotThrow( () -> {
+            demoUtils.throwException(5);
+        }, "Should not throw exception");
+    }
+
+    @Test
+    @DisplayName("Timeout")
+    void testTimeout() {
+        assertTimeoutPreemptively(Duration.ofSeconds(3), () -> {
+            demoUtils.checkTimeout();
+        }, "Method should execute in 3 seconds");
     }
 }
 
